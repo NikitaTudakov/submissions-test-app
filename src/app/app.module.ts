a61b2c42890c85d9ed4dd3217d998c25eb737b2d
NikitaTudakov <tudakov.nikita@gmail.com>
2022-10-21 11:13:19 +0300

    added based components, added default styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { SubmissionsComponent } from './components/submissions/submissions.component';
+import { GoogleMapComponent } from './components/google-map/google-map.component';
+import { SubmissionListComponent } from './components/submission-list/submission-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    SubmissionsComponent,
+    GoogleMapComponent,
+    SubmissionListComponent
   ],
   imports: [
     BrowserModule
